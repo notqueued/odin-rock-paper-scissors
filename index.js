838e47301021968d454aa02c197245048f34f294
notqueued <sorinmazilu1@gmail.com>
2021-12-16 18:48:07 +0200

    game logic with js

--- index.js
+++ index.js
@@ -13,63 +13,87 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lost! Paper beats Rock";
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lost! Scissors beats Paper";
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lost! Rock beats Scissors";
-    }
-    else if (playerSelection === computerSelection) {
-        return "It's a draw!"
-    } else {
-        return "You won! " + playerSelection.charAt(0).toUpperCase() + 
-        playerSelection.substring(1) + " beats " + computerSelection.charAt(0).toUpperCase() + 
-        computerSelection.substring(1);
-    }
-}
-
 function scoreBoard(points, roundResult) {
     if (roundResult === "won") {
-        points[0]++;
+        points++;
+        document.getElementsByClassName("scorePlayer")[0].textContent = String(points);
     } else if (roundResult === "lost") {
-        points[1]++;
+        points++;
+        document.getElementsByClassName("scoreComputer")[0].textContent = String(points);
     }
-    return points;
 }
 
-function game() {
-    let score = [0,0];
-    for(let i = 0; i < 5; i++){
-        let smth = computerPlay();
-        let player = window.prompt("Choose beetween: rock, paper or scissors");
-        let playerLower = player.toLowerCase();
-        while (playerLower !== "rock" && playerLower !== "paper" &&
-        playerLower !== "scissors") {
-            player = window.prompt("Choose beetween: rock, paper or scissors");
-        }
-        let round = playRound(playerLower, smth.toLowerCase());
-        if (round.indexOf("won") > -1){
-            score = scoreBoard(score,"won");
-        } else if (round.indexOf("lost") >-1) {
-            score = scoreBoard(score,"lost");
-        }
-        console.log(`Round ${i+1}:`);
-        console.log(`${playerLower[0].toUpperCase() + playerLower.substring(1)} vs ${smth}`);
-        console.log(round);
-        console.log(`Player: ${score[0]}`);
-        console.log(`Computer: ${score[1]}`);
+function playRound(playerSelection) {
+    let scorePlayer = updatePlayer();
+    let scoreComputer = updateComputer();
+    let computerSelection = computerPlay();
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("You lost! Paper beats Rock");
+        document.getElementsByClassName("resultText")[0].textContent = "You lost! Paper beats Rock";
+        scoreBoard(scoreComputer,"lost");
     }
-    if(score[0] > score[1]){
-        console.log("Congrats! You won!");
-    } else if (score[0] < score[1]) {
-        console.log("Too bad. You lost!");
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("You lost! Scissors beats Paper");
+        document.getElementsByClassName("resultText")[0].textContent = "You lost! Scissors beats Paper";
+        scoreBoard(scoreComputer,"lost");
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("You lost! Rock beats Scissors");
+        document.getElementsByClassName("resultText")[0].textContent = "You lost! Rock beats Scissors";
+        scoreBoard(scoreComputer,"lost");
+    }
+    else if (playerSelection === computerSelection) {
+        console.log("It's a draw!");
+        document.getElementsByClassName("resultText")[0].textContent = "It's a draw!";
     } else {
-        console.log("What a bummer. It's a draw!");
+        console.log("You won! " + playerSelection + " beats " + computerSelection);
+        document.getElementsByClassName("resultText")[0].textContent = "You won! " + playerSelection + " beats " + computerSelection;
+        scoreBoard(scorePlayer,"won");
+    }
+
+    scorePlayer = updatePlayer();
+    scoreComputer = updateComputer();
+    console.log(scorePlayer);
+    console.log(scoreComputer);
+    
+    if(scorePlayer === 5 || scoreComputer === 5) {
+        endOfGame(scorePlayer, scoreComputer);
     }
 }
 
-game();
+function updatePlayer() {
+    return parseInt(document.getElementsByClassName("scorePlayer")[0].textContent);
+}
+
+function updateComputer() {
+    return parseInt(document.getElementsByClassName("scoreComputer")[0].textContent);
+}
+
+function endOfGame(scorePlayer, scoreComputer){
+    if (scorePlayer > scoreComputer) {
+        document.getElementsByClassName("resultText")[0].textContent = "You won!";
+    } else {
+        document.getElementsByClassName("resultText")[0].textContent = "You lost!";
+    }
+    let selectionBtn = document.getElementsByClassName("selection-btn");
+    for (let i = 0; i < selectionBtn.length; i++){
+        selectionBtn[i].setAttribute("disabled", "");
+    }
+    const div = document.querySelector('.result');
+    const playAgain = document.createElement('button');
+    playAgain.classList.add('playAgain');
+    playAgain.textContent = "Play again";
+    playAgain.addEventListener('click', () => { newGame();});
+    div.appendChild(playAgain);
+}
+
+function newGame() {
+    let selectionBtn = document.getElementsByClassName("selection-btn");
+    for (let i = 0; i < selectionBtn.length; i++){
+        selectionBtn[i].removeAttribute("disabled");
+    }
+    document.querySelector('.scorePlayer').textContent = "0";
+    document.querySelector('.scoreComputer').textContent = "0";
+    document.querySelector('.resultText').textContent = "";
+    document.querySelector('.playAgain').remove();
+}
